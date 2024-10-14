cb4d2b57a3d5416837fdbba053678157f66e6b10
areeb0207 <areebsiddique02@gmail.com>
2024-10-15 01:27:42 +0530

    links & fix

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(' adrian@jsmastery.pro');
+    navigator.clipboard.writeText(' mohdareeb0207@gmail.com');
     setHasCopied(true);
 
     setTimeout(() => {
